50eecffc350306935d9a772b1aa8742cd975b031
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-01 02:06:10 +0600

    First commit with basic mongodb express setup

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.0o9qayn.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("BanglaBazar Shopping website is Running........");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
